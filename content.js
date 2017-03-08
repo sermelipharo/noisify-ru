b0cce4c61a788b417d9c86fe64a13be210e57911
Cathy Deng <cathyd125@gmail.com>
2017-03-08 15:14:05 -0800

    better search bar updates

--- content.js
+++ content.js
@@ -11,20 +11,50 @@ $(document).ready(function() {
         success: function(data) {
           random_terms = data.split("\n");
           // TODO: deal w/ react updates?
-          setTimeout(kickOff, 4000);
+          kickOff();
         }
      });
 });
 
 
 function kickOff(){
-  // TODO: find more robust way of identifying search box
-  search_box = $("input[placeholder='Search Facebook']").eq(0);
-
+  updateDummySearchPlaceholder()
   window.setInterval(updateSearchPlaceholder, 10000);
 }
 
-function updateSearchPlaceholder(){
+function updateDummySearchPlaceholder(){
   var term = random_terms[Math.floor(Math.random()*random_terms.length)];
-  search_box.attr("placeholder", "Search for something random! like..."+term);
+  // this is very hack-y
+  // need to update search bar more frequently right after page load b/c stuff is shifting
+  refreshDummySearch(term);
+  setTimeout(function(){refreshDummySearch(term);}, 1000);
+  setTimeout(function(){refreshDummySearch(term);}, 2000);
+  setTimeout(function(){refreshDummySearch(term);}, 3000);
+  setTimeout(function(){refreshDummySearch(term);}, 4000);
+  setTimeout(function(){refreshDummySearch(term);}, 5000);
+  setTimeout(function(){refreshDummySearch(term);}, 6000);
+  setTimeout(function(){refreshDummySearch(term);}, 7000);
+  setTimeout(function(){refreshDummySearch(term);}, 8000);
+  setTimeout(function(){refreshDummySearch(term);}, 9000);
+}
+function refreshDummySearch(term){
+  search_box = $("input[placeholder='Search Facebook']").eq(0);
+  search_box.attr("placeholder", "aren't you curious about..."+term.toUpperCase());
+}
+
+
+function updateSearchPlaceholder(){
+  search_box = find_search_box();
+  var term = random_terms[Math.floor(Math.random()*random_terms.length)];
+  search_box.attr("placeholder", "aren't you curious about..."+term.toUpperCase());
+}
+
+function find_search_box(){
+  search_box = $("input[aria-label='Search']").eq(0);
+  if(search_box.length==0){
+    // in case actual search bar hasn't loaded yet
+    return $("input[placeholder='Search Facebook']").eq(0);
+  } else {
+    return search_box;
+  };
 }
